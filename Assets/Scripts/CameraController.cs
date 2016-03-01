ddbae7056341d42f56d53f45406801645986f2c3
jbeauno <morinemm@yahoo.fr>
2016-03-01 16:51:42 +0100

    First Commit
    Unity demo project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject playerdemerde;
+
+    public GameObject Sol;
+
+    private Vector3 offset;
+	
+	void Start () {
+        offset = transform.position - playerdemerde.transform.position;        
+	}
+	
+	
+	void LateUpdate () {
+        transform.position = playerdemerde.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+
+    public float speed = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float mh = Input.GetAxis("Horizontal");
+        float mv = Input.GetAxis("Vertical");
+
+        Vector3 mov = new Vector3(mh, 0, mv);
+
+        rb.AddForce(mov * speed);
+    }
+}
